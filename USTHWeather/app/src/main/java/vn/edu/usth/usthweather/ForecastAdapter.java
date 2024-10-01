8f9c134209924c98625eeca47b8bdf8ee470a3b4
Your Name <you@example.com>
2024-10-01 22:31:05 +0700

    practical12

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastAdapter.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastAdapter.java
@@ -11,8 +11,8 @@ import java.util.List;
 
 public class ForecastAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
-    Context context;
-    List<ForecastItem> items;
+    private final Context context;
+    private final List<ForecastItem> items;
 
     public ForecastAdapter(Context context, List<ForecastItem> items) {
         this.context = context;
@@ -22,15 +22,17 @@ public class ForecastAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.forecast_item, parent, false));
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return new MyViewHolder(inflater.inflate(R.layout.forecast_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.day.setText(items.get(position).getDay());
-        holder.weather_icon.setImageResource(items.get(position).getWeather_icon());
-        holder.weather.setText((items.get(position).getWeather()));
-        holder.temperature.setText(items.get(position).getTemperature());
+        ForecastItem item = items.get(position);
+        holder.day.setText(item.getDay());
+        holder.weather_icon.setImageResource(item.getWeather_icon());
+        holder.weather.setText(item.getWeather());
+        holder.temperature.setText(item.getTemperature());
     }
 
     @Override
